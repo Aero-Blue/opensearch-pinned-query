777478b26114512b25174ffe7106111222a14c2d
David Hanft <hanfdavw@amazon.com>
2022-07-18 18:39:26 -0700

    Add pinned-query attribute to rest API query params

--- src/main/java/org/opensearch/pinnedquery/PinnedQueryPlugin.java
+++ src/main/java/org/opensearch/pinnedquery/PinnedQueryPlugin.java
@@ -7,9 +7,24 @@
  */
 package org.opensearch.pinnedquery;
 
+import org.opensearch.common.io.stream.Writeable;
+import org.opensearch.index.query.QueryBuilder;
+import org.opensearch.pinnedquery.query.PinnedQueryBuilder;
 import org.opensearch.plugins.Plugin;
+import org.opensearch.plugins.SearchPlugin;
+
+import java.util.List;
+
+import static java.util.Collections.singletonList;
 
 
-public class PinnedQueryPlugin extends Plugin {
-    // Implement the relevant Plugin Interfaces here
+public class PinnedQueryPlugin extends Plugin implements SearchPlugin {
+    @Override
+    public List<QuerySpec<?>> getQueries() {
+        return singletonList(
+                new QuerySpec<QueryBuilder>(
+                        PinnedQueryBuilder.NAME,
+                        PinnedQueryBuilder::new,
+                        PinnedQueryBuilder::fromXContent));
+    }
 }
